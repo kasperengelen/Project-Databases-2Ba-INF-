fa0526ef72e7af2004ae268f26c352fbc9dc4f21
Kasper Engelen <kasper.engelen@hotmail.com>
2018-02-25 15:55:56 +0100

    Login, registration forms, project restruct

--- webclient/client.py
+++ webclient/client.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-from flask_wtf import FlaskForm
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-from passlib.hash import sha256_crypt
+from flask import Flask, render_template, redirect, request
+import user_manager
+
 
 app = Flask(__name__)
 app.config.update(dict(
@@ -9,35 +8,17 @@ app.config.update(dict(
     WTF_CSRF_SECRET_KEY = "Uei\xc2&\x8a\x18.H\x87\xc5\x1d\xd1\xc8\xc3\xcf\xe5\xfft_\x8c:\x03r"
 ))
 
-class RegisterForm(FlaskForm):
-    firstname = StringField('Firstname', [validators.Length(min=1, max=50)])
-    lastname = StringField('Lastname', [validators.Length(min=1, max=50)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirmpwd', message='Passwords do not match')
-    ])
-    confirmpwd = PasswordField('Confirm Password')
-
-class LoginForm(Form):
-    email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [ validators.DataRequired() ])
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm(request.form)
-    return render_template('login.html', form = form)
+    return user_manager.login_user(request)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        return render_template('register.html')
-    return render_template('register.html', form = form)
+    return user_manager.register_user(request)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- webclient/user_manager.py
+++ webclient/user_manager.py
@@ -1,9 +1,43 @@
 # file that contains all code for managing users: registration, editing, etc.
+# SOURCE: wtforms documentation, stackoverflow and https://www.youtube.com/watch?v=zRwy8gtgJ1A&list=PLillGF-RfqbbbPz6GSEM9hLQObuQjNoj_&index=1
+
+from wtforms import StringField, PasswordField, validators
+from wtforms.validators import Length, InputRequired, Email, EqualTo
+
+class UserRegisterForm(FlaskForm):
+    """Class that represents a user registration form. The form has the following fields:
+    Firstname and lastname fiels. These need to be between 1 and 50 characters long. Both are required.
+    Email field, this needs to be between 6 and 70 characters long, is required and is checked with
+    a regex to be a valid email.
+    Password and Confirm password fields, these need to contain identical values, need to be between
+    6 and 50 characters long, and are required.
+    """
+    firstname = StringField('Firstname', [InputRequired(message="Firstname is required."),
+                                          Length(min=1, max=50, message="Firstname should contain between 1 and 50 characters.")])
+    lastname = StringField('Lastname', [InputRequired(message="Lastname is required."),
+                                        Length(min=1, max=50, message="Lastname should contain between 1 and 50 characters.")])
+    email = StringField('Email', [InputRequired(message="Email address is required."),
+                                  Email(message="The supplied email address is not of a valid format."),
+                                  Length(min=6, max=70, message="Email address should contain between 1 and 50 characters.")])
+    password = PasswordField('Password', [InputRequired("Password is required."),
+                                          EqualTo('passwordconfirm', message="Passwords do not match."),
+                                          Length(min=6, max=50, message="Password needs to be between 6 and 50 characters long.")])
+    passwordconfirm = PasswordField('Confirm Password')
 
 
+class UserLoginForm(FlaskForm):
+    """Class that represents a user login form. The form has a email field,
+    this field is checked with a regex to be of the format of an email address.
+    The supplied email needs to be between 6 and 70 characters long. The form also
+    has a password field."""
+    email = StringField('Email', [InputRequired(message="Email is required.")
+                                  Email(message="The supplied email address is not of a valid format."),
+                                  Length(min=6, max=70, message="Email ")])
+    password = PasswordField('Password', [DataRequired()])
 
 
-class UserRegisterForm(Form):
-	pass
+def login_user(request_data):
+    return render_template('index.html')
 
-
+def register_user(request_data):
+    return render_template('index.html')
