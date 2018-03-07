e72c5b03ae3f35bcb30cfa704a569e8fe91661e5
Student <uauser@lab02.uantwerpen.be>
2018-03-07 14:09:35 +0100

    Added the trigger to keep data clean after admin delete

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -40,3 +40,16 @@ CREATE TABLE set_permissions (
     CHECK(permission_type IN ('admin', 'write', 'read')),
     FOREIGN KEY(userid) REFERENCES user_accounts(userid) ON DELETE CASCADE
 );
+
+--TRIGGER to delete all the data if the admin is deleted (needs to be modified for more than 1 admin)
+/*CREATE FUNCTION delete_clean() RETURNS TRIGGER AS $BODY$
+BEGIN
+    DELETE FROM datasets WHERE datasets.setid IN
+    (SELECT set_permissions.setid FROM set_permissions WHERE
+    set_permissions.userid = OLD.userid AND set_permissions.permission_type = 'admin');
+    RETURN OLD;
+END;
+$BODY$ LANGUAGE PLPGSQL;
+ 
+CREATE TRIGGER deleted_user BEFORE DELETE ON user_accounts
+FOR EACH ROW EXECUTE PROCEDURE delete_clean();*/
