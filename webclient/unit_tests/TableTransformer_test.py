125ed6cf71df3d3bcb52979667502eb62598990f
chrisnsana <christ-offert.nsanamatundu@student.uantwerpen.be>
2018-03-22 14:28:42 +0100

    Unit testing added

---
+++ webclient/unit_tests/TableTransformer_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestTableTransformer(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def dummy_test(self):
+        self.assertEqual(1, 1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
