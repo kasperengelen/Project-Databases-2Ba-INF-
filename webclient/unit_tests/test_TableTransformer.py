9bf08c1a6b7a4d39c2e09abdb06e272ec0fb90ae
chrisnsana <christ-offert.nsanamatundu@student.uantwerpen.be>
2018-03-23 15:54:07 +0100

    Added better setUp's for my tests

--- webclient/unit_tests/test_TableTransformer.py
+++ webclient/unit_tests/test_TableTransformer.py
@@ -17,20 +17,34 @@ class TestTableTransformer(unittest.TestCase):
         cls.db_connection = psycopg2.connect("dbname='hmtbpols' user='hmtbpols' host='baasu.db.elephantsql.com' password='yIje-2zT-zF0YyJywkAy57h6ob3ZnoV2'")
         #Fake userid = 0 and And setid is actually the TEST set
         cls.test_object = transformer.TableTransformer(0, 'TEST', cls.db_connection, None, True)
-        cls.db_connection.cursor().execute("CREATE SCHEMA IF NOT EXISTS \"TEST\"")
+        cur = cls.db_connection.cursor()
+        cur.execute("CREATE SCHEMA IF NOT EXISTS \"TEST\"")
         cls.db_connection.commit()
-        creation_query = """CREATE TABLE IF NOT EXISTS "TEST".test_table (
+        creation_query = """CREATE TABLE "TEST".test_table (
         string VARCHAR(255) NOT NULL,
         number INTEGER NOT NULL,
         date_time VARCHAR(255) NOT NULL,
         garbage VARCHAR(255));"""
+        #In some cases the test fails in a way that tearDownClass is not called and the table still exists
+        #Sadly we can't confirm if the table is still correct, because of transformations performed on it
+        try:
+            cur.execute(creation_query)
 
-        cls.db_connection.cursor().execute(creation_query)
+        except psycopg2.ProgrammingError:
+            #If it was still present in the database we better drop the schema and rebuild it
+            cls.db_connection.rollback()
+            cur.execute("DROP SCHEMA \"TEST\" CASCADE")
+            cur.execute("CREATE SCHEMA \"TEST\"")
+            cls.db_connection.commit()
+            cur.execute(creation_query)
+            cls.db_connection.commit()
+            
+            
         values = [('C-Corp', 1, '08/08/1997'), ('Apple', 10, '01/04/1976'), ('Microsoft', 8, '04/04/1975') , ('Nokia', 3000, '12/05/1865') , ('Samsung', 7, '01/03/1938'),
                   ('Huawei', 4521, '15/09/1987'), ('Razer', 9000, '01/01/1998')]
 
         for v in values:
-            cls.db_connection.cursor().execute("INSERT INTO \"TEST\".test_table VALUES(%s, %s, %s)", v)
+            cur.execute("INSERT INTO \"TEST\".test_table VALUES(%s, %s, %s)", v)
 
         
         cls.db_connection.commit()
