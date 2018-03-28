f676cf032d5e10a7e17e3df7b16bb5136f24b3d2
Atisha Calazans <atisha.ribeirodecalazansduarte@student.uantwerpen.be>
2018-03-28 11:17:53 +0000

    fixed the bad fix of the small mistake

--- webclient/Controller/DataLoader.py
+++ webclient/Controller/DataLoader.py
@@ -14,7 +14,7 @@ class DataLoader:
         self.db_conn = db_wrapper.DBWrapper()
 
         # first check if the setid is valid
-        if DatasetManager.existsID(self.setid):
+        if DatasetManager.existsID(setid):
             self.setid = setid
         else:
             raise ValueError("setid is not valid")
