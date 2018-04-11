708c8638847d5418843caf1e86fc278bec8a0efd
chrisnsana <christ-offert.nsanamatundu@student.uantwerpen.be>
2018-04-11 05:07:39 +0200

    Fixed bug that Kasper found on page indexing and added cleaner documentation to TableViewer

--- webclient/Controller/TableViewer.py
+++ webclient/Controller/TableViewer.py
@@ -10,17 +10,26 @@ import db_wrapper
 from psycopg2 import sql
 
 class TableViewer:
+    """Class that extracts table information for viewing purposes.
 
-    def __init__(self, setid, tablename, engine):
+    Attributes:
+        setid: The id of the dataset that the user wants to modify
+        tablename: The name of the table we're extracting information from.
+        engine: SQLalchemy engine to use pandas functionality
+        db_connection: psycopg2 database connection to execute SQL queries
+    """
+
+    def __init__(self, setid, tablename, engine, db_connection=None):
         self.engine = engine
         self.setid = setid
         self.tablename = tablename
-        #query_result = pd.read_sql(sql_query, self.engine)
+        self.db_connection = db_connection
         self.maxrows = None
 
 
-    #Get all the attributes from a table
+
     def get_attributes(self):
+        """Method that returns a list of all attributes of the table."""
         SQL_query = "SELECT * FROM \"%s\".\"%s\" LIMIT 1" % (str(self.setid), self.tablename)
         data_frame = pd.read_sql(SQL_query, self.engine)
         return data_frame.columns.values.tolist()
@@ -28,6 +37,12 @@ class TableViewer:
     
     #Given a a number of rows to display this functions returns a list of possible page indices.
     def get_page_indices(self, display_nr, page_nr=1):
+        """Method that returns the relevant indices for a table that's being viewed.
+
+        Parameters:
+            display_nr: Integer specifying how much rows of a table have to be shown per page.
+            page_nr: Integer indicating which page we're viewing to extract the right rows.
+        """
         if self.maxrows is None:
             #This method will set the maxrows
             self.is_in_range(1, 1)
@@ -37,7 +52,7 @@ class TableViewer:
         max_index = math.ceil(table_size / display_nr)
         #At this point the table is too large to just show all the indices, we have to minimize clutter
         if(max_index > 5):
-            if page_nr > 2:
+            if page_nr > 3:
                 indices = ['1', '...', ]
                 start = page_nr
             else:
@@ -64,31 +79,44 @@ class TableViewer:
         return indices
 
 
-    #Method that calculates whether a index is in range of table with display of nr_rows.
-    #Make sure get_page_indices is always called before this method for properly setting self.maxrows
     def is_in_range(self, page_nr, nr_rows):
+        """Method that returns True if a page index is in range and False if it's not in range.
+
+        Parameters:
+            page_nr: Integer indicating which page we're trying to view
+            nr_rows: The number of rows that are being showed per page. 
+        """
         count_query  = "SELECT COUNT(*) FROM \"%s\".\"%s\"" % (self.setid, self.tablename)
         query_result = pd.read_sql(count_query, self.engine)
         table_size = query_result.iat[0, 0]
         self.maxrows = table_size
             
         if((page_nr - 1) * nr_rows >= self.maxrows):
-            #In case it's an empty table
-            if table_size == 0:
+            #In case it's an empty table, the first page should still be in range
+            if (table_size == 0) and (page_nr == 1):
                 return True
             else:
                 return False
         else:
             return True
 
-    #This method returns a html table representagtion given the setid, tablename and page number and how many rows per page
+
     def render_table(self, page_nr, nr_rows):
+        """This method returns a html table representing the page of the SQL table.
+
+        Parameters:
+            page_nr: Integer indicating which page we're viewing.
+            nr_rows: The number of rows that are being showed per page. 
+        """
         offset = 0
         offset = (page_nr - 1) * nr_rows
         SQL_query = "SELECT * FROM \"%s\".\"%s\" LIMIT %s OFFSET %s" % (str(self.setid), self.tablename, nr_rows, offset)
         data_frame = pd.read_sql(SQL_query, self.engine)
         html_table = re.sub(' mytable', '" id="mytable', data_frame.to_html(None, None, None, True, False, classes='mytable'))
         return html_table
+
+
+    
 
     def to_csv(self, foldername, delimiter=',', quotechar='"', null="NULL"):
         """Convert a table from the dataset to a CSV file. The csv file will be stored
@@ -123,7 +151,7 @@ class TableViewer:
 
     def get_most_frequent_value(self, columnname):
         """Return the value that appears most often in the column"""
-        conn = get_db()
+        conn = get_db() # = self.db_connection
 
         # get the frequence of every value and select the one that has the highest one
         conn.cursor().execute(sql.SQL("SELECT {}, COUNT(*) FROM {}.{} GROUP BY {} ORDER BY COUNT(*) DESC,"
@@ -136,7 +164,7 @@ class TableViewer:
 
     def get_null_frequency(self, columnname):
         """Return the amount of times NULL appears in the column"""
-        conn = get_db()
+        conn = get_db() # = self.db_connection
 
         conn.cursor().execute(sql.SQL("SELECT {}, COUNT(*) FROM {}.{} WHERE {} IS NULL GROUP BY {}"
                                       "").format(sql.Identifier(columnname), sql.Identifier(str(self.setid)),
@@ -147,7 +175,7 @@ class TableViewer:
 
     def __aggregate_function(self, columnname, aggregate):
         """Wrapper that returns result of aggregate function"""
-        conn = get_db()
+        conn = get_db() # = self.db_connection
 
         conn.cursor().execute(sql.SQL("SELECT " + aggregate + "({}) FROM {}.{}").format(sql.Identifier(columnname),
                                                                           sql.Identifier(str(self.setid)),
