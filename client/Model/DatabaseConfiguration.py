e47d47c14990a24a62fb87175b2e00095983849e
chrisnsana <christ-offert.nsanamatundu@student.uantwerpen.be>
2018-05-05 16:49:28 +0200

    Made a TestConnection class and finished testing it.

--- client/Model/DatabaseConfiguration.py
+++ client/Model/DatabaseConfiguration.py
@@ -1,27 +1,23 @@
 import configparser
 import io
 import os
-from Model.DBWrapper import DBWrapper
 import psycopg2
 import psycopg2.pool
 from sqlalchemy import create_engine
 
 
-class DatabaseConfiguration:
-    """Class following the singleton pattern that reads and stores the database configuration of the project.
-    The data of the configuration can be safely accessed through the public methods of this class.
-    
-    """
+class _DatabaseLoader:
+    """Internal class that loads information about a database connection from a config file and creates all
+    the necessary resources to make use of the DB."""
 
-    __instance = None
 
     class __InnerClass:
-        def __init__(self, dbname, user, host, password, t_connection):
+        def __init__(self, dbname, user, host, password, release):
             self.dbname = dbname
             self.user = user
             self.host = host
             self.password = password
-            self.test_connection = t_connection
+            self.release = release
             self.connection_pool = psycopg2.pool.SimpleConnectionPool(5, 20, dbname=dbname, user=user, password=password, host=host)
             self.engine = create_engine("postgresql://{}:{}@{}/{}".format(self.user, self.password, self.host, self.dbname),
                                          pool_size=20, max_overflow=0)
@@ -51,7 +47,7 @@ class DatabaseConfiguration:
 
         def is_test_connection(self):
             """Returns a boolean indicating whether you're connected to the testing database."""
-            return self.test_connection
+            return not self.release
         
         def get_db2(self):
             """Returns a psycopg2 database connection from the connection pool."""
@@ -72,63 +68,84 @@ class DatabaseConfiguration:
             """
             self.connection_pool.putconn(db_conn)
             
-
-        def close(self):
-            """Closes all internal resources."""
-            self.connection_pool.closeall()
-            self.engine.dispose()
             
 
-    def __init__(self, release=True):
-        if DatabaseConfiguration.__instance is None:
-            try: #Try opening the file and loading it
-                #Get path without worrying about current working directory.
-                if release is False:
-                    path = os.path.dirname(os.path.abspath(__file__)) + '/../test_config.ini'
-                else:
-                    path = os.path.dirname(os.path.abspath(__file__)) + '/../config.ini'
-                    
-                with open(path) as conf:
-                    db_config = conf.read()
-            
-            except(FileNotFoundError):
-                raise FileNotFoundError("The config file config.ini was not found!"
-                                        " Please add this file to allow proper configuration"
-                                        " of the database connection.")
-            
-            config = configparser.RawConfigParser(allow_no_value=True)
-            config.read_file(io.StringIO(db_config))
+    def __init__(self):
+        if type(self) == _DatabaseLoader:
+            raise Exception('DatabaseLoader should not be directly constructed. '
+                            'Use DatabaseConfiguration or TestConnection instead.')
 
-            #Extract content to store it the datamembers
-            db_info = config.sections()[0] #The only relevant section is the first one
-            values =  {}
-            fields = ['db_name', 'user', 'host', 'password']
-            conf_fields = config.options(db_info)
-            
-            for e in fields: #Let's check if all the needed fields are present in the config file
-                if e not in conf_fields:
-                    error_msg = 'Error! The config file is missing the field "' + e + '"'
-                    raise ValueError(error_msg)
+    def load_db(self, release):
+        """Read database details and create the internal object and return it."""
+        try: #Try opening the file and loading it
+            #Get path without worrying about current working directory.
+            if release is False:
+                path = os.path.dirname(os.path.abspath(__file__)) + '/../test_config.ini'
+            else:
+                path = os.path.dirname(os.path.abspath(__file__)) + '/../config.ini'
+                
+            with open(path) as conf:
+                db_config = conf.read()
+        
+        except(FileNotFoundError):
+            raise FileNotFoundError("The config file config.ini was not found!"
+                                    " Please add this file to allow proper configuration"
+                                    " of the database connection.")
+        
+        config = configparser.RawConfigParser(allow_no_value=True)
+        config.read_file(io.StringIO(db_config))
 
-                values[e] = config.get(db_info, e)
-                if values[e] == "": #We can't have empty values in our config file so an exception will be raised
-                    error_msg = 'Error! The value for the field "' + e + '"can not be empty!'
-                    raise ValueError(error_msg)
-            
-            DatabaseConfiguration.__instance = self.__InnerClass(values['db_name'], values['user'], values['host'], values['password'], release)
+        #Extract content to store it the datamembers
+        db_info = config.sections()[0] #The only relevant section is the first one
+        values =  {}
+        fields = ['db_name', 'user', 'host', 'password']
+        conf_fields = config.options(db_info)
+        
+        for e in fields: #Let's check if all the needed fields are present in the config file
+            if e not in conf_fields:
+                error_msg = 'Error! The config file is missing the field "' + e + '"'
+                raise ValueError(error_msg)
 
-
-    def close(self):
-        """Close all the connections of the DB configuration and dispose of the singleton object.
-        Subsequent call to the constructor will create the new singleton object.
-        """
-        self.__instance.close()
-        self.__instance = None
+            values[e] = config.get(db_info, e)
+            if values[e] == "": #We can't have empty values in our config file so an exception will be raised
+                error_msg = 'Error! The value for the field "' + e + '"can not be empty!'
+                raise ValueError(error_msg)
+        
+        new_obj = self.__InnerClass(values['db_name'], values['user'], values['host'], values['password'], release)
+        return new_obj
 
 
+class DatabaseConfiguration(_DatabaseLoader):
+    """Class following the singleton pattern that reads and stores the database configuration of the project.
+    The data of the configuration can be safely accessed through the public methods of this class.
+    """
+
+    __instance = None
+
+    def __init__(self):
+        if self.__instance is None:
+            fresh_obj = super().load_db(True)
+            DatabaseConfiguration.__instance = fresh_obj
 
     def __getattr__(self, name):
         return getattr(self.__instance, name)
+
+class TestConnection(_DatabaseLoader):
+    """Class following the singleton pattern that reads and stores the database configuration of the project.
+    The data of the configuration can be safely accessed through the public methods of this class.
+    This class is used to connect to the testing database for testing purposes.
+    """
+
+    __instance = None
+
+    def __init__(self):
+        if self.__instance is None:
+            fresh_obj = super().load_db(False)
+            TestConnection.__instance = fresh_obj
+
+    def __getattr__(self, name):
+        return getattr(self.__instance, name)
+        
     
 
 
