1b51ccd6941892b2753d16b3193de6c616faf553
AtishaCalazans <atisha.ribeirodecalazansduarte@student.uantwerpen.be>
2018-05-21 13:08:33 +0200

    changed DatasetDownloader workings

--- client/Model/DatasetDownloader.py
+++ client/Model/DatasetDownloader.py
@@ -4,6 +4,7 @@ import os
 import zipfile
 import shutil
 from Model.DatabaseConfiguration import DatabaseConfiguration
+from Model.QueryManager import QueryManager
 
 class DatasetDownloader():
     """Class that reads tables from a schema and puts them into a file"""
@@ -12,6 +13,7 @@ class DatasetDownloader():
         self.db_connection = db_connection
         self.cur = self.db_connection.cursor()
         self.schema = str(setid)
+        self.query_man = QueryManager(self.db_connection, None)
 
     def get_csv(self, tablename, foldername, delimiter=',', quotechar='"', null="NULL", original=False):
         """Convert a table from the dataset to a CSV file. The csv file will be stored
@@ -44,6 +46,8 @@ class DatasetDownloader():
             # write rows
             outcsv.writerows(rows)
 
+        return tablename + ".csv"
+
     def get_csv_zip(self, foldername, delimiter=',', quotechar='"', null="NULL", original=False):
         """Converts all tables in a dataset to csv and puts them in a zip file called (setid).zip
         If original is true, the zip will contain folder 'original' and 'edited'"""
@@ -67,6 +71,7 @@ class DatasetDownloader():
         shutil.make_archive(os.path.join(foldername + "/" + self.schema), 'zip', csv_temp_folder)
 
         shutil.rmtree(csv_temp_folder)
+        return self.schema + ".zip"
 
     def get_table_dump(self, tablename, foldername, original=False):
         """Create a dump file with name (tablename).dump and puts it in 'foldername'"""
@@ -79,25 +84,47 @@ class DatasetDownloader():
             dumpfile.write("CREATE TABLE {};\n".format(tablename))
             self.__insert_values(dumpfile, schema, tablename)
 
+        return tablename + ".dump"
+
     def get_dataset_dump(self, foldername, original=False):
         """Create a dump file with name (setid).dump and puts it in 'foldername'"""
 
-        schema = self.__get_schema(original)
-
         # fetch all tablenames
-        self.cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = %s;",
-                         [schema])
-        result = self.cur.fetchall()
-        table_names = [t[0] for t in result]
+        table_names = self.query_man.get_table_names(self.schema)
 
-        filename = os.path.join(foldername, schema + ".dump")
-        with open(filename, 'w') as dumpfile:
+        dumpfolder = foldername
+        # only used if original == True
+        og_schema = self.__get_schema(original)
+
+        if original:
+            og_table_names = self.query_man.get_table_names(og_schema)
+            dumpfolder = os.path.join(foldername, "temp_folder")
+            os.mkdir(dumpfolder)
+
+        with open(os.path.join(dumpfolder, self.schema + ".dump"), 'w') as dumpfile:
             # create table statements
             for table in table_names:
                 dumpfile.write("CREATE TABLE {};\n".format(table))
 
             for table in table_names:
-                self.__insert_values(dumpfile, schema, table)
+                self.__insert_values(dumpfile, self.schema, table)
+
+        if not original:
+            return self.schema + ".dump"
+        else:
+            with open(os.path.join(dumpfolder, og_schema + ".dump"), 'w') as dumpfile:
+                # create table statements
+                for table in og_table_names:
+                    dumpfile.write("CREATE TABLE {};\n".format(table))
+
+                for table in og_table_names:
+                    self.__insert_values(dumpfile, og_schema, table)
+
+            # make a zip of all csv's
+            shutil.make_archive(os.path.join(foldername + "/" + self.schema), 'zip', dumpfolder)
+
+            shutil.rmtree(dumpfolder)
+            return self.schema + ".zip"
 
     def __create_csvs(self, foldername, delimiter=',', quotechar='"', null="NULL", original=False):
         """Converts all tables in a dataset to csv and puts them in (foldername)"""
