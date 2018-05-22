a7c743246fcc80bd7c9c5e307bbf948f1aee20ad
Kasper Engelen <kasper.engelen@hotmail.com>
2018-05-22 16:29:27 +0200

    dedup page render

--- client/View/dataset_pages.py
+++ client/View/dataset_pages.py
@@ -1099,5 +1099,8 @@ def transform_dedup_find_matches(dataset_id, tablename):
     ignore_list = form.ignore_list.data
     exactmatch_list = form.exactmatch_list.data
 
-    return dd.find_matches(dataset_id, tablename, exactmatch_list, ignore_list)
+    table_list = dd.find_matches(dataset_id, tablename, exactmatch_list, ignore_list)
+
+    render_template('dataset_pages.deduplication.html', table_list = table_list,
+                                                        attributes = tv.get_attributes())
 # ENDFUNCTION
