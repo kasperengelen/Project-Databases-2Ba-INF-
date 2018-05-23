1de6562306b6e052108d0f9aa1c7a9cb2270a954
Student <uauser@lab07.uantwerpen.be>
2018-05-23 18:50:20 +0200

    Working on undo in DHM

--- client/Controller/DatasetHistoryManager.py
+++ client/Controller/DatasetHistoryManager.py
@@ -90,13 +90,13 @@ class DatasetHistoryManager:
         """
         return False
 
-    def get_last_transformation(self, tablename):
-        """
+    def get_latest_backup(self, tablename):
         cur = self.db_connection.cursor()
-        cur.execute(sql.SQL('SELECT tablename FROM system.dataset_history'
-                            ' WHERE table_name = %s))')3
-        """
-        
+        values = [self.setid, tablename]
+        cur.execute(sql.SQL('SELECT table_name FROM system.dataset_history WHERE transformation_id =
+                            '(SELECT MAX(transformation_id) FROM system.dataset_history WHERE setid = %s
+                            ' AND origin_table = %s AND transformation_type = -1)'), values)
+        return cur.fetchone()[0]
         
     def __python_list_to_postgres_array(self, py_list, transformation_type):
         """Method that represents a python list as a postgres array for inserting into a PostreSQL database."""
