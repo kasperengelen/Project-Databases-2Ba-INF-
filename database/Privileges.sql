f449b0540a2006e964a18a0a575d266717defb44
Kasper Engelen <kasper.engelen@hotmail.com>
2018-05-23 21:21:36 +0200

    updated DB privileges

--- database/Privileges.sql
+++ database/Privileges.sql
@@ -6,6 +6,7 @@ GRANT USAGE, SELECT ON SEQUENCE system.user_accounts_userid_seq TO dbadmin;
 GRANT USAGE, SELECT ON SEQUENCE system.dataset_history_transformation_id_seq TO dbadmin;
 
 
+GRANT ALL PRIVILEGES ON DATABASE testdb TO dbadmin;
 GRANT CREATE ON DATABASE testdb TO dbadmin;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA system TO dbadmin;
 GRANT ALL PRIVILEGES ON SCHEMA system TO dbadmin;
